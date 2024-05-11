5dfd7bdd8da906db4cc1825bda35c34aaf9eeab3
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-11 11:08:45 -0500

    abc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//other stuffs
 const path = require("path")
 const fs = require('fs')
 const scratch = require("./scratch.js")
@@ -7,5 +8,38 @@ fs.mkdirSync("../dist")
 
 var project = new scratch.project()
 
+var costume1 = new scratch.costume(fs.readFileSync("../costumes/backdrop.svg"))
+var backdrop = new scratch.target({isStage:true,name:"Stage"})
+backdrop.json.costumes.push(costume1.json)
+project.json.targets.push(backdrop.json)
+costume1.save(path.resolve("../dist"))
+
+var costume2 = new scratch.costume(fs.readFileSync("../costumes/cat.svg"))
+var sprite1 = new scratch.target({isStage:false,name:"Jim"})
+sprite1.json.costumes.push(costume2.json)
+project.json.targets.push(sprite1.json)
+costume2.save(path.resolve("../dist"))
+
 
 fs.writeFileSync("../dist/project.json",JSON.stringify(project.json),"utf-8")
+
+
+//finally commit archive
+var archiver = require('archiver');
+
+function zipDirectory(sourceDir, outPath) {
+    const archive = archiver('zip', { zlib: { level: 9 }});
+    const stream = fs.createWriteStream(outPath);
+  
+    return new Promise((resolve, reject) => {
+      archive
+        .directory(sourceDir, false)
+        .on('error', err => reject(err))
+        .pipe(stream)
+      ;
+  
+      stream.on('close', () => resolve());
+      archive.finalize();
+    });
+  }
+  zipDirectory(path.resolve("../dist"),path.resolve("../project.sb3"))
--- src/scratch.js
+++ src/scratch.js
@@ -1,13 +1,79 @@
+const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+const fs = require("fs")
+const path = require("path")
+
 class project{
     constructor(options={}){
 
-        this.json = {targets:[],monitors:[],extensions:[]}
+        this.json = {targets:[],monitors:[],extensions:[],"meta": {
+            "semver": "3.0.0",
+            "vm": "2.3.0",
+            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
+        }}
     }
 }
+let layer = 0
 class target{
     constructor(options={}){
-        
+        this.json = {
+            isStage:true,
+            name:"Stage",
+            variables:{},
+            lists:{},
+            broadcasts:{},
+            blocks:{},
+            comments:{},
+            currentCostume:0,
+            costumes:[],
+            sounds:[],
+            layerOrder:layer,
+            volume:100,
+            tempo:1,
+        }
+        layer++
+        Object.keys(options).forEach(key=>{
+            this.json[key] = options[key]
+        })
+        if(this.json.isStage == true){
+            this.json = Object.assign(this.json,{
+                videoState:"on",
+                videoTransparency:50,
+                textToSpeechLanguage:null
+            })
+        }else{
+            this.json = Object.assign(this.json,{
+                visible:true,
+                x:0,
+                y:0,
+                size:100,
+                direction:90,
+                draggable:false,
+                rotationStyle:"all around"
+            })
+        }
+    }
+}
+class costume{
+    constructor(data,options={}){
+        this.data = data
+        this.id = genRanHex(32)
+        this.json = {
+            name: "costume1",
+            dataFormat: "svg",
+            assetId: this.id,
+            md5ext: "temp",
+            rotationCenterX: 0,
+            rotationCenterY: 0,
+            "bitmapResolution": 1,
+        }
+        this.json.md5ext = this.id+"."+this.json.dataFormat
+        Object.keys(options).forEach(key=>{
+            this.json[key] = options[key]
+        })
+    }
+    save(dirPath){
+        fs.writeFileSync(path.join(dirPath,this.json.md5ext),this.data)
     }
 }
 
-module.exports = {project,target}
+module.exports = {project,target,costume}

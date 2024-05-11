34140dcced6414a36772a62e8dbdbe16951788b9
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-05-11 10:05:51 -0500

    e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const path = require("path")
 const fs = require('fs')
+const scratch = require("./scratch.js")
 
-if(fs.existsSync("../dist"))fs.rmdirSync("../dist")
+if(fs.existsSync("../dist"))fs.rmSync("../dist",{recursive:true})
 fs.mkdirSync("../dist")
+
+var project = new scratch.project()
+
+
+fs.writeFileSync("../dist/project.json",JSON.stringify(project.json),"utf-8")
---
+++ src/scratch.js
@@ -0,0 +1,13 @@
+class project{
+    constructor(options={}){
+
+        this.json = {targets:[],monitors:[],extensions:[]}
+    }
+}
+class target{
+    constructor(options={}){
+        
+    }
+}
+
+module.exports = {project,target}
